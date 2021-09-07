fd13b0b370d553f3281e990b1feb9c622eb607c5
sirageDb <ilovesemicolons@hotmail.com>
2021-09-07 18:03:32 +0200

    + Navigator/Router of the applicatoin

--- App.tsx
+++ App.tsx
@@ -1,21 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import React from "react";
+import AuthScreen from "./src/screens/AuthScreen";
+import FlashCard from "./src/screens/Flashcard";
+import ListFlashCards from "./src/screens/ListFlashCards";
+import { enableScreens } from "react-native-screens";
+import {screenNames} from "./src/utils/screenNames"
+
+enableScreens();
+
+import { NavigationContainer, ParamListBase } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator<ParamListBase>();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name={screenNames.authScreen.name}
+          component={AuthScreen}
+          options={{ title: screenNames.authScreen.title }}
+        />
+        <Stack.Screen
+          name={screenNames.listFlashCards.name}
+          component={ListFlashCards}
+          options={{ title: screenNames.listFlashCards.title }}
+        />
+        <Stack.Screen
+          name={screenNames.flashCard.name}
+          component={FlashCard}
+          options={{title : screenNames.flashCard.title}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
