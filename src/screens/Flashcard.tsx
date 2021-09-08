f7f253bf9b152be69af28d827afcc184049a756e
sirageDb <ilovesemicolons@hotmail.com>
2021-09-08 10:43:24 +0200

    * typing * dynamic content

--- src/screens/Flashcard.tsx
+++ src/screens/Flashcard.tsx
@@ -1,28 +1,65 @@
-import React, { useEffect } from "react";
-import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
 import { RouteProp, useRoute } from "@react-navigation/core";
-import { RootStackParamList } from "../utils/types";
+import { iFlashCard, RootStackParamList, Subtitle } from "../utils/types";
 import { useNavigation } from "@react-navigation/core";
-import { FlashCardProps } from "../utils/types";
 import AppLayout from "../components/Layout";
-
+import { flashCardMock } from "../utils/mocks/flachCardMock";
+import SubTitleSection from "../components/SubTitleSection";
+import { ScrollView } from "react-native-gesture-handler";
 //========================================================
-export default function FlashCard({
-  user_id,
-  flashCard_id,
-  flashCardTitle,
-}: FlashCardProps): JSX.Element {
+export default function FlashCard(): JSX.Element {
+  const [flashCard, setFlashCard] = useState<iFlashCard>();
+
   const route = useRoute<RouteProp<RootStackParamList, "FlashCard">>();
   const navigation = useNavigation<any>();
 
   useEffect(() => {
+    setFlashCard(flashCardMock);
     navigation.setOptions({ title: route.params.flashCardTitle });
   });
 
   return (
     <AppLayout>
-      <Text>{flashCardTitle}</Text>
-      <Text>this is a title</Text>
+      <ScrollView>
+        <View style={styles.tagsContainer}>
+          {flashCard?.tag.map((tag: any, i: number) => {
+            return (
+              <Text style={styles.tag} key={i}>
+                #{tag}
+              </Text>
+            );
+          })}
+        </View>
+        <View>
+          {flashCard?.subtitle.map((subtitle: Subtitle, i: number) => {
+            return (
+              <View style={styles.subTitleContainer} key={i}>
+                <SubTitleSection
+                  key={i}
+                  title={subtitle.title}
+                  paragraphs={subtitle.paragraph}
+                />
+              </View>
+            );
+          })}
+        </View>
+      </ScrollView>
     </AppLayout>
   );
 }
+
+const styles = StyleSheet.create({
+  tagsContainer: {
+    flexDirection: "row",
+    marginBottom: 10,
+    flexWrap: "wrap",
+  },
+  tag: {
+    marginRight: 20,
+  },
+
+  subTitleContainer: {
+    marginBottom: 20,
+  },
+});
