e3a713e9654f20698d64b7305bc6b1901499af3e
sirageDb <ilovesemicolons@hotmail.com>
2021-09-08 10:46:21 +0200

    + fixed existing types * added new flashCard - deleted unnecessary flashCardCell types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,16 +1,45 @@
-export type FlashCardProps = {
-    user_id: string,
-    flashCard_id : string,
-    flashCardTitle : string
-}
+
 export type FlashCardCellProps = {
-    flashCardTitle : string,
     flashCard_id : string
+    flashCardTitle : string,
 }
 
+//=============================================================
+export type Paragraph = {
+    text : string
+}
+
+export type Subtitle = {
+    title : string,
+    position : number,
+    paragraph : Paragraph[],
+}
+
+type Ressource  = {
+    name : string,
+    url : string,
+}
+
+export interface iFlashCard {
+    _id : string,
+    title  : string,
+    tag : string[],
+    subtitle : Subtitle[]
+    ressource : Ressource[]
+}
+//=============================================================
 export type RootStackParamList  = {
     FlashCard : FlashCardCellProps
 }
+//=============================================================
+export interface iSubTitleSectionProps {
+    title : string,
+    paragraphs : Paragraph[]
+}
+//=============================================================
+
+
+
 
 
 
