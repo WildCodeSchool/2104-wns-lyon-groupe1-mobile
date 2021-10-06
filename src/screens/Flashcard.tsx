92ea41e968d1ef457725618c84808b5530fc0a42
sirageDb <ilovesemicolons@hotmail.com>
2021-10-06 20:06:34 +0200

    CHORE : flash card is now pre filled with content since now api calls should be done incase user is offline

--- src/screens/Flashcard.tsx
+++ src/screens/Flashcard.tsx
@@ -4,19 +4,23 @@ import { RouteProp, useRoute } from "@react-navigation/core";
 import { iFlashCard, RootStackParamList, Subtitle } from "../utils/types";
 import { useNavigation } from "@react-navigation/core";
 import AppLayout from "../components/Layout";
-import { flashCardMock } from "../utils/mocks/flachCardMock";
 import SubTitleSection from "../components/SubTitleSection";
 import { ScrollView } from "react-native-gesture-handler";
 import RessourcesSection from "../components/ResourcesSection";
 //========================================================
 export default function FlashCard(): JSX.Element {
-  const [flashCard, setFlashCard] = useState<iFlashCard>();
-
   const route = useRoute<RouteProp<RootStackParamList, "FlashCard">>();
+
+  const [flashCard, setFlashCard] = useState<iFlashCard>({
+    title: route.params.flashCardTitle,
+    _id: route.params.flashCard_id,
+    ressource: route.params.flashCardRessources,
+    tag: route.params.flashCardTags,
+    subtitle: route.params.subtitles,
+  });
   const navigation = useNavigation<any>();
 
   useEffect(() => {
-    setFlashCard(flashCardMock);
     navigation.setOptions({ title: route.params.flashCardTitle });
   });
 
@@ -36,16 +40,12 @@ export default function FlashCard(): JSX.Element {
           {flashCard?.subtitle.map((subtitle: Subtitle, i: number) => {
             return (
               <View style={styles.subTitleContainer} key={i}>
-                <SubTitleSection
-                  key={i}
-                  title={subtitle.title}
-                  paragraphs={subtitle.paragraph}
-                />
+                <SubTitleSection key={i} title={subtitle.title} paragraphs={subtitle.paragraph} />
               </View>
             );
           })}
 
-        <RessourcesSection ressources={flashCard?.ressource!}/>
+          <RessourcesSection ressources={flashCard?.ressource!} />
         </View>
       </ScrollView>
     </AppLayout>
